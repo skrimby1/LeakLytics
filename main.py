c7691bbb2a021c493b2d34741ef4c31f9c5f2755
skrimby1 <114155106+skrimby1@users.noreply.github.com>
2025-04-09 22:08:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,32 @@
-# version=1.0.0
+# version=1.0.1
 from microwifimanager.manager import WifiManager
 from adc_sub import ADC_substitute
 import umail
 import urequests as requests
 import ujson
-from machine import Pin, PWM
+from machine import Pin, PWM, deepsleep
 import uos
+import gc
+from time import sleep
 
 wlan = WifiManager().get_connection()
 
 if wlan is None:
-    if config_btn.value() == 0: #Pull-up modstand
-        wlan
+    print("Could not initialize the network connection.")
     while True:
-        if config_btn.value() == 0: #Pull-up modstand
-            wlan
-        opening_snd()
-        pass  #is connected
+        pass  # you shall not pass :D 
 
 ###################Pins########################################
-config_btn = Pin(4, Pin.IN, Pin.PULL_UP)
-adc_pin = Pin(3, Pin.IN)
+#config_btn = Pin(14, Pin.IN, Pin.PULL_UP)
+#battery_pin = Pin(4, Pin.IN)
+adc_pin = Pin(35, Pin.IN)
 adc = ADC_substitute(adc_pin)
 buzzer_pin = Pin(5, Pin.OUT)
+#battery_adc= ADC_substitute(battery_pin)
+#alarm_led = Pin(8, Pin.OUT)
+#alarm_led.off()
 buzzer_pwm = PWM(buzzer_pin, duty=0)
+#stop_alarm_btn = Pin(7, Pin.IN)
 
 ###########Variables###############################
 with open("config.json", "r") as file:
@@ -33,16 +36,15 @@ with open("config.json", "r") as file:
     whatsapp = data["whatsapp"]
     device_id = data["device_id"]
 
-sender_email = "notification@leaklytics.com"
 
-smtp = umail.SMTP('smtp.gmail.com', 587, username='notification@leaklytics.com', password='mypassword')
+#smtp = umail.SMTP('smtp.gmail.com', 465, ssl=True)
 
-receive_email = smtp.to('rosterloader@gmail.com')
 
 whatsapp_batmsg10 = f'WARNING%21%20LeakLytics%20device%20%7B{device_id}%7D%20is%20running%20low%20on%20battery%2C%20please%20change%20the%20battery%20as%20soon%20as%20possible%21' #YOUR MESSAGE HERE (URL ENCODED)https://www.urlencoder.io/
 
 whatsapp_batmsg5 = f'WARNING%2521%2520LeakLytics%2520device%2520%257B{device_id}%257D%2520is%2520running%2520low%2520on%2520battery%252C%2520please%2520change%2520the%2520battery%2520as%2520soon%2520as%2520possible%2521'
 
+alarm_msg = f'ATTENTION%20-%20LEAK%20DETECTED%20FROM%20%7Bdevice_id%7D.%20Please%20check%20the%20area%20immediately%20to%20prevent%20potential%20water%20damage.%20Notifications%20will%20keep%20coming%20until%20%22%22stop%20alarm%22%22%20button%20has%20been%20pressed.'
 #Juster udfra endelige microcontroller, er forskellig fra esp til ESP - det her er udfra ESP32 C3 SUPER MINI
 a = ((100 - 0) / (4095 - 2414))
 b = 0 - (a * 2414)
@@ -61,6 +63,17 @@ def send_message(phone_number, api_key, message):
     print('Error')
     print(response.text)
 
+def leak_alarm():
+    while True:
+        smtp.write("Subject: Water Leak Detected – Immediate Attention Required\n\n")
+        smtp.send(f"ATTENTION - LEAK DETECTED FROM {device_id}. Please check the area immediately to prevent potential water damage. Notifications will keep coming until ""stop alarm"" button has been pressed. ")
+        send_message(whatsapp, alarm_msg, whatsapi)
+        machine.lightsleep(600)
+        alarm_led.on
+        if stop_alarm_btn.value() == 0:
+            smtp.send(f"Good news! The leak reported by {device_id} has been cleared. Everything's back to normal.")
+            alarm_led.off
+            machine.reset()
 #-----------------------Updating-----------------------------------
 def get_version_from_script(script):
     """Extract version number from the script."""
@@ -70,13 +83,14 @@ def get_version_from_script(script):
     return None
 
 def check_for_update():
-    url = "https://raw.githubusercontent.com/your_username/your_repo/branch_name/main.py"  # Your raw GitHub URL
+    url = "https://raw.githubusercontent.com/skrimby1/LeakLytics/refs/heads/main/main.py"  # Your raw GitHub URL
     print("Checking for update from:", url)
 
     # Send GET request to fetch the file content
     response = requests.get(url)
     if response.status_code == 200:
         new_script = response.text
+        print(new_script)
         new_version = get_version_from_script(new_script)
 
         if new_version != CURRENT_VERSION:
@@ -113,22 +127,27 @@ def buzzer(pwm_object, frequency, tone_duration, silence_duration):
     sleep(silence_duration)
 
 def opening_snd():
-    buzzer(buzzer_pwm, 800, 0.3, 0)
-    buzzer(buzzer_pwm, 850, 0.3, 0)
-    buzzer(buzzer_pwm, 900, 0.3, 0)
-    buzzer(buzzer_pwm, 1000, 0.5, 0)
-    buzzer(buzzer_pwm, 1200, 0.2, 0)
-#---------------------------------Main-------------------------------------------------------------
+    buzzer(buzzer_pwm, 659, 0.15, 0.05)
+    buzzer(buzzer_pwm, 659, 0.15, 0.05)  
+    buzzer(buzzer_pwm, 659, 0.15, 0.05)  
+    buzzer(buzzer_pwm, 523, 0.15, 0.05)  
+    buzzer(buzzer_pwm, 392, 0.30, 0)      
 
+#---------------------------------Main-------------------------------------------------------------
+opening_snd()
 #Battery notification proccess
 while True:
-    adc
     reading_adc = adc.read_adc()
-    batteriprocent = a * reading_adc + b
+    #reading_battery = battery_adc.read_adc()
+    batteriprocent = a * reading_battery + b
     batteriprocent
     if batteryprocent < 10:
-        smtp.send(f"WARNING! LeakLytics device {device_name} has 10% charge left, please change the battery as soon as possible!")
+        smtp.send(f"WARNING! LeakLytics device {device_id} has 10% charge left, please change the battery as soon as possible!")
         send_message(whatsapp, whatsapi, whatsapp_batmsg10)
     if batteryprocent < 5:
-        smtp.send(f"WARNING! LeakLytics device {device_name} is at a CRITICAL 5% battery level, battery needs to be changed now or else leaks won't be detected soon!") 
+        smtp.send(f"WARNING! LeakLytics device {device_id} is at a CRITICAL 5% battery level, battery needs to be changed now or else leaks won't be detected soon!") 
         send_message(whatsapp, whatsapi, whatsapp_batmsg5)
+    if reading_adc == 4095:
+        leak_email()
+    #machine.deepsleep(900)
+machine.reset()
